2b16e2b1978582bf8846e83f0ba774e9147fc9fd
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2019-10-16 05:04:03 -0700

    Update Controller.java

--- src/main/java/codejava/webapp/Controller.java
+++ src/main/java/codejava/webapp/Controller.java
@@ -11,19 +11,26 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/controller")
 public class Controller extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		 PrintWriter out= resp.getWriter();
-			String trigerFrom = req.getParameter("bookButton");
-			if (trigerFrom.equals("check")) 
-			{
-			String opt= req.getParameter("Currency");
-			String num=req.getParameter("num");
+		 	String trigerFrom = req.getParameter("bookButton");
+			String opt="";
+			String num="";
+		if (trigerFrom.equals("check")) 
+		{
+			opt= req.getParameter("Currency");
+			num=req.getParameter("num");
 			int n=Integer.parseInt(num);
 			Bean b=new Bean(opt,n);
 			b.curr();
-			out.println("<h1>"+num+"Equivalent Indian Rupee is"+b.getRes()+"</h1>");
-			out.close();
-			}
-
+			try{		
+				PrintWriter out= resp.getWriter();
+				out.println("<h1>"+num+"Equivalent Indian Rupee is"+b.getRes()+"</h1>");
+				out.close();
+			     }
+			catch(Exception e)
+			   {
+			      e.printStackTrace();
+				}
+		}
 	} 
 
 }
