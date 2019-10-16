50ccfaf283862003f4afd4fa4ef30983451f94ed
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2019-10-16 05:09:11 -0700

    Update Controller.java

--- src/main/java/codejava/webapp/Controller.java
+++ src/main/java/codejava/webapp/Controller.java
@@ -11,13 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/controller")
 public class Controller extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		 	String trigerFrom = req.getParameter("bookButton");
-			String opt="";
-			String num="";
+		 	
+		String trigerFrom = req.getParameter("bookButton");	
 		if (trigerFrom.equals("check")) 
 		{
-			opt= req.getParameter("Currency");
-			num=req.getParameter("num");
+		    	String opt= req.getParameter("Currency");
+		    	String num=req.getParameter("num");
 			int n=Integer.parseInt(num);
 			Bean b=new Bean(opt,n);
 			b.curr();
@@ -25,5 +24,5 @@ public class Controller extends HttpServlet {
 			out.println("<h1>"+num+"Equivalent Indian Rupee is"+b.getRes()+"</h1>");
 			out.close();			
 		} 
-
+	}
 }
