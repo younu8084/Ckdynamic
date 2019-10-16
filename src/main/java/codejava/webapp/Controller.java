e6325f004189e207fa2d8471671cdea37c423e24
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2019-10-16 04:12:51 -0700

    Update Controller.java

--- src/main/java/codejava/webapp/Controller.java
+++ src/main/java/codejava/webapp/Controller.java
@@ -8,10 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class Controller
- */
 @WebServlet("/controller")
 public class Controller extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
