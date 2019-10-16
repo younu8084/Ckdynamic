14b3d2d462c39fcdfcd54085f2c086c949d413e2
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2019-10-16 04:49:43 -0700

    Update Controller.java

--- src/main/java/codejava/webapp/Controller.java
+++ src/main/java/codejava/webapp/Controller.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/controller")
 public class Controller extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		  try
-        { 
 		 PrintWriter out= resp.getWriter();
 			String trigerFrom = req.getParameter("bookButton");
 			if (trigerFrom.equals("check")) {
@@ -25,10 +23,6 @@ public class Controller extends HttpServlet {
 			out.close();
 			}
 
-	} }
-	catch(IOException ex) 
-        { 
-            System.out.println("Exception caught in catch block"); 
-        } 
+	} 
 
 }
